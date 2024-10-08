b554bab0d4c8c548ffbb3348e667c9c995e597a3
Aman Dileep <amancfcmbb44@gmail.com>
2024-10-08 19:57:33 +0530

    06-01-authcontext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,13 @@ import viteLogo from '/vite.svg'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
 import Home from './pages/home/Home'
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import {Toaster} from 'react-hot-toast'
+import { useAuthContext } from './context/AuthContext'
 
 function App() {
+
+  const {authUser} = useAuthContext()
 
   return (
     <div className='
@@ -15,9 +18,9 @@ function App() {
     '>
       <Toaster/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/' element={authUser ? <Home/> : <Navigate to="/login"/>}/>
+        <Route path='/login' element={authUser ? <Navigate to="/"/> : <Login/>}/>
+        <Route path='/signup' element={authUser ? <Navigate to="/"/> : <Signup/>}/>
       </Routes>
     </div>
   )
--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import toast from 'react-hot-toast';
+import { useAuthContext } from '../context/AuthContext';
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const {setAuthUser} = useAuthContext();
 
   const signup = async ({fullname, username, password, confirmPassword, gender}) => {
     const success = handleInputError({fullname, username, password, confirmPassword, gender});
@@ -22,13 +24,19 @@ const useSignup = () => {
             })
         })
         const data = await res.json();
-        console.log(data);
+        if (data.error){
+            throw new Error(data.error);
+        }
+
+        localStorage.setItem("chat-user", JSON.stringify(data));
+        setAuthUser(data);
     } catch (error){
         toast.error(error.messsage);
     } finally{
         setLoading(false);
     }
   }
+
 
   return { loading, signup };
 }
